41caf80f931c87d8c7ba56554712ec18cc069ed7
benkatzav <katzavben@gmail.com>
2022-01-19 02:57:06 +0200

    Initial Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pizza Recipe', 'Description for Pizza', 'https://assets.bonappetit.com/photos/5cd32ef32c3537178c3c8f03/6:9/w_1630,h_2445,c_limit/BA-Perfect-Pizza.jpg'),
+    new Recipe('Shnitzel Recipe', 'Description for Shnitzel', 'https://assets.bonappetit.com/photos/57ae1afd53e63daf11a4e26f/1:1/w_1920,c_limit/chicken-schnitzel.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
